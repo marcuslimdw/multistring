2c9b9291676b8d032978814947c9324233be6023
Marcus Lim <marcuslimdw@gmail.com>
2023-02-09 09:21:44 +0800

    Add documentation to multistring package

--- multistring.go
+++ multistring.go
@@ -1,3 +1,5 @@
+// Package multistring provides wrappers around the standard library's strings package's functions that take string
+// slices.
 package multistring
 
 import (
@@ -6,10 +8,12 @@ import (
 
 type Wrap []string
 
+// JoinBy joins the strings in the `Wrap` using a separator.
 func (w Wrap) JoinBy(sep string) string {
 	return strings.Join(w, sep)
 }
 
+// MapString applies a function over the strings in the Wrap, returning a new Wrap.
 func (w Wrap) MapString(mapping func(string) string) Wrap {
 	result := make(Wrap, len(w))
 	for i, s := range w {
